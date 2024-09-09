da8fbc0588b6b44f8d6ef88a72a14f00f40643dd
Jari Jokinen <jari@j2digital.io>
2024-09-09 16:40:38 +0300

    Remove default plugins

--- src/init-j2digital-wp.sh
+++ src/init-j2digital-wp.sh
@@ -2,9 +2,17 @@
 
 set -e
 
-FOREGROUND=0 ./init-wp.sh
+DISABLE_FOREGROUND=1 ./init-wp.sh
 
-ln -s /wp-theme/dist /wp/wp-content/themes/j2digital
+if [[ ! -e /wp/wp-content/themes/j2digital ]]; then
+  ln -s /wp-theme/dist /wp/wp-content/themes/j2digital
+fi
+
+chown -R wp:wp /wp/wp-content/themes/j2digital
+
+rm -rf /wp/wp-content/themes/twenty*
+rm -rf /wp/wp-content/plugins/akismet
+rm -rf /wp/wp-content/plugins/hello.php
 
 tail -f /dev/null
 
